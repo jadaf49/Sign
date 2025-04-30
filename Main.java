8947512fbaae663723dbfe733bcbac4af8f50ee3
jadaf49 <jadaf49@nycstudents.net>
2025-04-30 16:27:37 +0000

    fixed getLines and numberofLines methods & ensured all test cases were successful

--- Main.java
+++ Main.java
@@ -2,9 +2,25 @@ public class Main
 {
     public static void main(String[]args)
     {
-        int x;
-        Sign sign1=new Sign("ABC222DE",3); //should print ABC;222;DE
+        Sign sign1=new Sign("ABC222DE",3); 
         System.out.println(sign1.numberofLines()); //should print 3
+        System.out.println(sign1.getLines()); // should print ABC;222;DE
+        
+        Sign sign2=new Sign("ABCD",10);
+        System.out.println(sign2.numberofLines());
+        System.out.println(sign2.getLines());
+
+        Sign sign3=new Sign("ABCDEF",6);
+        System.out.println(sign3.numberofLines());
+        System.out.println(sign3.getLines());
+
+        Sign sign4=new Sign("",4);
+        System.out.println(sign4.numberofLines());
+        System.out.println(sign4.getLines());
+
+        Sign sign5=new Sign("AB_CD_EF",2);
+        System.out.println(sign5.numberofLines());
+        System.out.println(sign5.getLines());
 
     }
 }
--- Sign.java
+++ Sign.java
@@ -10,11 +10,21 @@ public class Sign
 
     public int numberofLines()
     {
-        return (message.length()/width)+1; // the length dividided by width gives # of full lines but add 1 for wtv is leftover
+        int numlines=message.length()/width;
+        if (message.length()%width==0) return numlines;
+        return numlines+1; // the length dividided by width gives # of full lines but add 1 for wtv is leftover
     }
 
     public String getLines()
     {
-        return null;
+        if (message.length()==0) return null;
+        String s="";
+        int i=0;
+        while (i<message.length()){
+            s+=message.substring(i,i+1);
+            i++;
+            if (i%width==0 && i!=message.length())s+=";";
+        }
+        return s;
     }
 }
